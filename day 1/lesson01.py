bb8b99b2245b5e75d3f291ca73ff4b439c514d64
andriaGoa <andriagithub1@gmail.com>
2024-05-16 21:13:18 +0400

    second commit

---
+++ day 1/lesson01.py
@@ -0,0 +1,3 @@
+name = "andria"
+surname = "chankvetadze"
+age = "12"
--- day26/classwork/day26.classwork.py
+++ day26/classwork/day26.classwork.py
@@ -1,12 +1,8 @@
-arrnumbers = []
-
+numbers = []
 for i in range(10, 50 + 1):
     numbers.append(i)
-
-
 def func(numbers):
     for index in range(len(numbers) - 1, -1, -1):
         if numbers[index] % 4 == 0:
             return numbers[index]
-
 print(func(numbers))
